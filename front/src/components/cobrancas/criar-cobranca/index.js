c3bd7596eb9d416b3df8e78d8b038fa3d7fd3861
neaar3 <neaar3@gmail.com>
2020-12-01 19:49:56 -0300

    fix: fix folders name and order

--- front/src/components/cobrancas/criar-cobranca/index.js
+++ front/src/components/cobrancas/criar-cobranca/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import React from "react";
 import pic from "../assets/pic.svg";
-import { BarraLateral } from "../barra lateral";
+import { BarraLateral } from "../../barra lateral";
 import CurrencyInput from "./CurrencyInput";
 
 
---
+++ front/src/components/cobrancas/criar-cobranca/listar-cobrancas/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+import React from "react";
+import pic from "../assets/pic.svg";
+import { BarraLateral } from "../../barra lateral";
+import CurrencyInput from "./CurrencyInput";
