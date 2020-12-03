48500b165e97878adabebc77549633f3bac071f5
neaar3 <neaar3@gmail.com>
2020-12-03 02:34:28 -0300

    fix: fix some itens

--- front/src/App.js
+++ front/src/App.js
@@ -5,12 +5,13 @@ import { ListarCobranca } from "./container/cobrancas/listar-cobrancas";
 import { ListarClientes } from "./container/clientes/listar-clientes";
 import { AdicionarCliente } from "./container/clientes/adicionar-clientes";
 import { EditarCliente } from "./container/clientes/editar-clientes";
+import { Home } from "./container/home";
 
 function App() {
   
   return (
     <div>
-      <EditarCliente></EditarCliente>
+      <Home></Home>
     </div>
   );
 }
---
+++ front/src/container/home/index.js
@@ -0,0 +1,69 @@
+import "./style.css";
+import React from "react";
+import pic from "../../assets/pic.svg";
+import vector from "../../assets/Vector.svg";
+import money from "../../assets/money.svg";
+import users from "../../assets/users.svg";
+import { BarraLateral } from "../../components/barra lateral";
+
+export function Home() {
+    return (
+        <div className="main">
+            <BarraLateral></BarraLateral>
+            <div className="conteudo">
+                <div className="header">
+                    <div className="saldo">
+                        <div>
+                            <img src={vector} className="cifrao" />
+                            <span>Saldo em conta</span>
+                        </div>
+                        <span className="saldo-em-conta">R$ 0,00</span>
+                    </div>
+                    <img className="perfil" src={pic} alt="foto do perfil de usuário" />
+                </div>
+                <div className="card-clientes">
+                    <div className="header-clientes">
+                        <img src={users} />
+                        Clientes
+                    </div>
+                    <div className="em-dia-card">
+                        <span>
+                            Em dia
+                        </span>
+                        <span className="valor-em-dia-card"> 0 </span>
+                    </div>
+                    <div className="inadimplentes-card">
+                        <span>
+                            Inadimplentes
+                        </span>
+                        <span className="valor-inadimplentes-card"> 0 </span>
+                    </div>
+                </div>
+                <div className="card-cobrancas">
+                    <div className="header-cobrancas">
+                        <img src={money} />
+                        Cobranças
+                    </div>
+                    <div className="previstas-card">
+                        <span>
+                            Previstas
+                        </span>
+                        <span className="valor-previstas-card"> 0 </span>
+                    </div>
+                    <div className="vencidas-card">
+                        <span>
+                            Vencidas
+                        </span>
+                        <span className="valor-vencidas-card"> 0 </span>
+                    </div>
+                    <div className="pagas-card">
+                        <span>
+                            Pagas
+                        </span>
+                        <span className="valor-pagas-card"> 0 </span>
+                    </div>
+                </div>
+             </div>
+        </div>
+    );
+}
