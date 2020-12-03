627e8f4b3851a865c648ed922c322cc6447712cc
Thauan Costa <thauan.dcosta@gmail.com>
2020-12-03 19:18:31 -0300

    feat: add  react-rout-dom to project

--- front/src/App.js
+++ front/src/App.js
@@ -6,12 +6,23 @@ import { ListarClientes } from "./container/clientes/listar-clientes";
 import { AdicionarCliente } from "./container/clientes/adicionar-clientes";
 import { EditarCliente } from "./container/clientes/editar-clientes";
 import { Home } from "./container/home";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   
   return (
     <div>
-      <ListarClientes></ListarClientes>
+		<BrowserRouter>
+		<Switch>
+			{/* <Route exact path='/login' component={Login} /> */}
+			<Route exact path='/' component={Home} />
+			<Route exact path='/cobrancas' component={ListarCobranca} />
+			<Route exact path='/clientes' component={ListarClientes} />
+			<Route exact path='/cobrancas/criar' component={CriarCobranca} />
+		</Switch>
+		</BrowserRouter>
+      
+
     </div>
   );
 }
