5655d49aa87357e436093c59ef222da3ca4eed4b
Thauan Costa <thauan.dcosta@gmail.com>
2020-12-03 04:06:31 -0300

    refactor: componentized header.

---
+++ front/src/components/header/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './style.css';
+
+import vector from '../../assets/Vector.svg'
+import pic from '../../assets/pic.svg'
+
+export function Header(props) {
+    const [click, setClick] = useState(true)
+    return (<div className={props.className}>
+        {props.name ? <span className="title-header">{props.name}</span>  : <div className="saldo">
+            <div>
+                <img src={vector} className="cifrao" />
+                <span>Saldo em conta</span>
+            </div>
+            <span className="saldo-em-conta">R$ 0,00</span>
+        </div>}
+<div className="perfil"><img onClick={ () => setClick(!click)} className="perfil" src={pic} alt="foto do perfil de usuário" />
+{!click ? <div>desloga</div>: undefined}</div>
+        
+    </div>)
+}
