dc5cc08cc920055cb4d8b1366af2fef8deb386e9
Thauan Costa <thauan.dcosta@gmail.com>
2020-12-03 20:03:59 -0300

    style: add style on logout button.

--- front/src/components/header/index.js
+++ front/src/components/header/index.js
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 
-import vector from '../../assets/Vector.svg'
-import pic from '../../assets/pic.svg'
-
+import vector from "../../assets/Vector.svg";
+import pic from "../../assets/pic.svg";
+import logout from '../../assets/log-out.svg'
 export function Header(props) {
-    const [click, setClick] = useState(true)
-    return (<div className={props.className}>
-        {props.name ? <span className="title-header">{props.name}</span>  : <div className="saldo">
-            <div>
-                <img src={vector} className="cifrao" />
-                <span>Saldo em conta</span>
-            </div>
-            <span className="saldo-em-conta">R$ 0,00</span>
-        </div>}
-<div className="perfil"><img onClick={ () => setClick(!click)} className="perfil" src={pic} alt="foto do perfil de usuário" />
-{!click ? <div>desloga</div>: undefined}</div>
-        
-    </div>)
+  const [click, setClick] = useState(true);
+
+  return (
+    <div className={props.className}>
+      {props.name ? (
+        <span className="title-header">{props.name}</span>
+      ) : (
+        <div className="saldo">
+          <div>
+            <img src={vector} className="cifrao" />
+            <span>Saldo em conta</span>
+          </div>
+          <span className="saldo-em-conta">R$ 0,00</span>
+        </div>
+      )}
+      <div className="perfil">
+        <img
+          onClick={() => setClick(!click)}
+          className="perfil"
+          src={pic}
+          alt="foto do perfil de usuário"
+        />
+        {!click ? (
+          <div className='logout'>
+			  <img src={logout} />
+            <button>
+              Deslogar
+            </button>
+			{/* onClick={() => setToken(null)} className="btnLogin" */}
+          </div>
+        ) : undefined}
+      </div>
+    </div>
+  );
 }
