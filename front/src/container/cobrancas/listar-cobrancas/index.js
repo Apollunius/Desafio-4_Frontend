54e567aa0c5d5f89c911569571391460399b94d6
neaar3 <neaar3@gmail.com>
2020-12-02 23:18:50 -0300

    refact: changes after some vscode problems

--- front/src/container/cobrancas/listar-cobrancas/index.js
+++ front/src/container/cobrancas/listar-cobrancas/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import pic from "../../../assets/pic.svg";
 import search from "../../../assets/search.svg";
 import vector from "../../../assets/Vector.svg";
+import printer from "../../../assets/printer.svg";
 import { BarraLateral } from "../../../components/barra lateral";
 
 export function ListarCobranca() {
@@ -28,23 +29,41 @@ export function ListarCobranca() {
                     </button>
                 </form>
                 <table>
-                    <tr>
-                        <td>Cliente</td>
-                        <td>Descrição</td>
-                        <td>Valor</td>
-                        <td>Status</td>
-                        <td>Vencimento</td>
-                        <td>Boleto</td>
-                    </tr>
-                    <tr>
-                        <td>teste</td>
-                        <td>teste</td>
-                        <td>teste</td>
-                        <td>teste</td>
-                        <td>teste</td>
-                        <td>teste</td>
-                    </tr>
+                    <thead>
+                        <tr className="tabela-header">
+                            <th>Cliente</th>
+                            <th>Descrição</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                            <th>Vencimento</th>
+                            <th>Boleto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="tabela-body">
+                            <td>Nome do cliente</td>
+                            <td>Descricao</td>
+                            <td>R$1000,00</td>
+                            <td>PENDENTE</td>
+                            <td>12/12/2020</td>
+                            <td><img src={printer}/></td>
+                        </tr>
+                    </tbody>
                 </table>
+                <div className="mudar-pagina">
+                    <a href="#" className="pagina voltar">
+                        &#60
+                    </a>
+                    <a href="#" className="pagina">
+                        1
+                    </a>
+                    <a href="#" className="pagina">
+                        2
+                    </a>
+                    <a href="#" className="pagina avancar">
+                        &#62
+                    </a>
+                </div>
              </div>
         </div>
     );
