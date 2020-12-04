fdef90b89c4abb757b09974954c3c1666852061c
neaar3 <neaar3@gmail.com>
2020-12-04 00:44:33 -0300

    refactor: clients list with data test

--- front/src/container/clientes/listar-clientes/index.js
+++ front/src/container/clientes/listar-clientes/index.js
@@ -1,18 +1,53 @@
 import "./style.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import { BarraLateral } from "../../../components/barra lateral";
 import { Button } from "../../../components/button";
 import { Header } from "../../../components/header";
-
-import pic from "../../../assets/pic.svg";
 import search from "../../../assets/search.svg";
-import vector from "../../../assets/Vector.svg";
 import email from "../../../assets/email.svg";
 import tel from "../../../assets/tel.svg";
 import edit from "../../../assets/edit.svg";
+import { useStores } from "../../../context";
+
 
 export function ListarClientes() {
+
+const [offset, setOffset] = React.useState(0)
+const [busca, setBusca] = React.useState("");
+const [dadosClientes, setDadosClientes] = React.useState("")
+const { token } = useStores();
+
+
+React.useEffect(()=>{
+    if(!busca) {
+        fetch(`http://localhost:8081/clientes?clientesPorPagina=10&offset=${offset}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token && `Bearer ${token}`,
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.dados.clientes)
+            setDadosClientes(data.dados.clientes);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    } else {
+        fetch(`http://localhost:8081/clientes?busca=${busca}&clientesPorPagina=10&offset=${offset}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.dados.clientes)
+            setDadosClientes(data.dados.clientes);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
+}, [offset])
+
   return (
     <div className="conteudo">
       <Header className="header-branco"></Header>
@@ -47,31 +82,36 @@ export function ListarClientes() {
           </tr>
         </thead>
         <tbody className="computar-tabela-cliente">
-          <tr className="tabela-body">
-            <td>
-              <tr className="tabela-cliente-itens">
-                <td className="tabela-cliente-nome">
-                  Nome e Sobrenome da Cliente
-                </td>
-                <td>
-                  <img src={email} />
-                  email@email.io
-                </td>
-                <td>
-                  <img src={tel} />
-                  (DDD) 4004-4828
-                </td>
-              </tr>
-            </td>
-            <td>R$1000,00</td>
-            <td>R$1000,00</td>
-            <td>EM DIA</td>
-            <td>
-              <Link to="/clientes/editar">
-                <img src={edit} />
-              </Link>
-            </td>
-          </tr>
+        {console.log(dadosClientes)}
+        { dadosClientes.map((element) => {
+            return (                                
+                <tr className="tabela-body">
+                    <td>
+                        <tr className="tabela-cliente-itens">
+                            <td className="tabela-cliente-nome">
+                                {element.nome}
+                            </td>
+                            <td>
+                                <img src={email} />
+                                {element.email}
+                            </td>
+                            <td>
+                                <img src={tel} />
+                                {element.tel}
+                            </td>
+                        </tr>
+                    </td>
+                    <td>R$1000,00</td>
+                    <td>R$1000,00</td>
+                    <td>EM DIA</td>
+                    <td>
+                    <Link to="/clientes/editar">
+                        <img src={edit} />
+                    </Link>
+                    </td>
+                </tr>)
+            })
+        }
         </tbody>
       </table>
       <div className="mudar-pagina">
